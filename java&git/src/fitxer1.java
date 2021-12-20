a132316e9aa29c0e45d5176eb4ee2632780bfba2
AbelSturm <a.sturm@sapalomera.cat>
2021-12-20 11:50:24 +0100

    Canvis en fitxer1 i creacio de fitxer2

--- java&git/src/fitxer1.java
+++ java&git/src/fitxer1.java
@@ -1,5 +1,6 @@
 public class fitxer1 {
     public static void main(String[] args) {
         System.out.println("HOLA QUE TAL");
+        System.out.println("MOLT BÉ");
     }
 }
---
+++ java&git/src/fitxer2.java
@@ -0,0 +1,5 @@
+public class fitxer2 {
+    public static void main(String[] args) {
+        
+    }
+}
